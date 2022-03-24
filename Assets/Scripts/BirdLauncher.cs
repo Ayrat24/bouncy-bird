5b9f4f8942a55d8195cc9943549127e25a7e75a9
Ayrat Xayaliev <ayrat.2498@gmail.com>
2022-03-24 21:57:18 +0500

    Uploaded project files

---
+++ Assets/Scripts/BirdLauncher.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdLauncher : MonoBehaviour
+{
+    [SerializeField] float forceMultiplier = 1;
+    [SerializeField] float minimumForce = 10;
+    [SerializeField] float maxPullDistance = 2;
+    [SerializeField] PlayerController player;
+    [SerializeField] Transform playerHolder;
+    [SerializeField] LineRenderer lineRenderer;
+    [SerializeField] float lineLength = 1f;
+    [SerializeField] float lineSpeedAdjuster = 0.2f;
+
+    private bool launcherActive;
+    private Camera camera;
+
+    void Start()
+    {
+        PutPlayerIntoLauncher();
+        camera = Camera.main;
+        lineRenderer.enabled = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            PutPlayerIntoLauncher();
+        }
+
+        if (!launcherActive)
+            return;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            lineRenderer.enabled = true;
+        }
+
+            if (Input.GetMouseButton(0))
+        {
+            Vector3 mousePos = camera.ScreenToWorldPoint(Input.mousePosition);
+            mousePos.z = 0;
+
+            Vector3 clampedPosition = Vector3.ClampMagnitude(mousePos - playerHolder.position, maxPullDistance);
+            player.transform.position = clampedPosition + playerHolder.position;
+
+            float seconsToPredict = lineLength;
+            float step = 0.1f;
+            int numberOfSteps = (int)(seconsToPredict / step);
+
+            lineRenderer.positionCount = numberOfSteps;
+            Vector3[] linePositions = new Vector3[numberOfSteps];
+            for(int i = 0; i < numberOfSteps; i++)
+            {
+                Vector3 force = ((playerHolder.position - player.transform.position)
+                * Vector2.Distance(playerHolder.position, player.transform.position) * forceMultiplier);
+                linePositions[i] = GetPositionInTime(step * i, player.transform.position, force * lineSpeedAdjuster);
+            }
+            lineRenderer.SetPositions(linePositions);
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            lineRenderer.enabled = false;
+
+            launcherActive = false;
+            player.EnableRigidBody();
+
+            Vector3 force = ((playerHolder.position - player.transform.position)
+                * Vector2.Distance(playerHolder.position, player.transform.position) * forceMultiplier);
+
+            player.AddForce(force);
+        }
+    }
+
+    public void PutPlayerIntoLauncher()
+    {
+        player.DisableRigidBody();
+        player.transform.position = playerHolder.position;
+
+        launcherActive = true;
+    }
+
+    private Vector2 GetPositionInTime(float time, Vector2 initialPosition, Vector2 initialSpeed)
+    {
+        return initialPosition + new Vector2(initialSpeed.x * time, initialSpeed.y * time - 4.905f * 3 * (time * time));
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rigidbody;
+    private BirdLauncher birdLauncher;
+
+    [SerializeField] float secondsToReturnIntoLauncher = 2f;
+    [SerializeField] float minSpeed = 1f;
+    float elapsedTimeWhileMinSpeed;
+    bool shouldStop;
+    [SerializeField] float playerSpeed;
+
+    void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        birdLauncher = FindObjectOfType<BirdLauncher>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!rigidbody.simulated)
+            return;
+
+        playerSpeed = rigidbody.velocity.magnitude;
+
+        if (rigidbody.velocity.magnitude < minSpeed)
+        {
+            elapsedTimeWhileMinSpeed += Time.deltaTime;
+        }
+        else
+        { 
+            elapsedTimeWhileMinSpeed = 0; 
+        }
+
+        if(elapsedTimeWhileMinSpeed >= secondsToReturnIntoLauncher)
+        {
+            elapsedTimeWhileMinSpeed = 0;
+            shouldStop = true;
+        }
+    }
+
+    public void AddForce(Vector3 force)
+    {
+        rigidbody.AddForce(force);
+    }
+
+    public void DisableRigidBody()
+    {
+        rigidbody.velocity = Vector3.zero;
+        rigidbody.angularVelocity = 0;
+        rigidbody.simulated = false;
+    }
+
+    public void EnableRigidBody()
+    {
+        rigidbody.simulated = true;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (shouldStop)
+        {
+            shouldStop = false;
+
+            Vector3 launcherPos = transform.position;
+            launcherPos.y += 2f;
+            birdLauncher.transform.position = launcherPos;
+
+            birdLauncher.PutPlayerIntoLauncher();
+        }
+    }
+}
